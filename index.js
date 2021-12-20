ec67eec15404863381254129decc8011b7b77938
Josh <66495148+Joshwgaming@users.noreply.github.com>
2021-12-20 17:13:21 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, Intents } = require('discord.js');
+const { botToken, prefix } = require('./config.json');
+const client = new Client({
+    intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    ]
+});
+
+client.login(botToken)
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}.`);
+});
+
+client.on('messageCreate', (message) => {
+
+    if (message.content === "hi") {
+        message.reply("hello")
+    }
+
+    if (message.content.startsWith(prefix)) {
+        const args = message.content.slice(prefix.length).split(' ');
+        let command = args[0];
+        if (command === "ping") {
+            message.reply("pong!")
+        }
+    }
+})
