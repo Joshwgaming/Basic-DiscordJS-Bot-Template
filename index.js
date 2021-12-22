1115b2a2b22815bd9e82e503ee0859c20857dd9b
Josh <66495148+Joshwgaming@users.noreply.github.com>
2021-12-22 17:39:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { Client, Intents } = require('discord.js');
 const { botToken, prefix } = require('./config.json');
 const client = new Client({
     intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
     ]
 });
 
